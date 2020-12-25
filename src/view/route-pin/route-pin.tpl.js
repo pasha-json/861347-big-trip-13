47bf9eb7151e2439242302f2ca256d8b328af089
Семён <hereff@ya.ru>
2020-12-25 13:56:03 +0300

    Сделал, чтобы звездочка "избранное" реагировала на данные

--- src/view/route-pin/route-pin.tpl.js
+++ src/view/route-pin/route-pin.tpl.js
@@ -17,7 +17,7 @@ const renderOptions = (points) => {
 
 
 export const createRoutePinTemplate = (points) => {
-  const {type, destination, options, date, price} = points;
+  const {type, destination, options, date, price, isFavourite} = points;
   const {start, end} = date;
   const startDateDay = dayjs(start).format(`MMM DD`);
   const startDateTime = dayjs(start).format(`HH:MM`);
@@ -47,7 +47,7 @@ export const createRoutePinTemplate = (points) => {
     </p>
     <h4 class="visually-hidden">Offers:</h4>
     ${optionNode}
-    <button class="event__favorite-btn event__favorite-btn--active" type="button">
+    <button class="event__favorite-btn ${isFavourite ? `event__favorite-btn--active` : ``}" type="button">
       <span class="visually-hidden">Add to favorite</span>
       <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
         <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
