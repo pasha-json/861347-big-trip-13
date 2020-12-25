4bf18c1edf5c612ef06d33a71c303dc7e4f0f1c9
Семён <hereff@ya.ru>
2020-12-25 13:58:20 +0300

    Добавил в utils/render функции remove и replace

--- src/utils/render.js
+++ src/utils/render.js
@@ -32,3 +32,30 @@ export const renderElement = (container, child, place) => {
       break;
   }
 };
+
+export const remove = (component) => {
+  if (!(component instanceof Abstract)) {
+    throw new Error(`Can remove only components`);
+  }
+
+  component.getElement().remove();
+  component.removeElement();
+};
+
+export const replace = (newChild, oldChild) => {
+  if (oldChild instanceof Abstract) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof Abstract) {
+    newChild = newChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (oldChild === null || newChild === null || parent === null) {
+    throw new Error(`Can't replace unexisting elements`);
+  }
+  parent.replaceChild(newChild, oldChild);
+
+};
